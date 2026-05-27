512d52c6abebad8abdf124e2c339193f30ad0b03
yuvarajgit-2505 <yuvaraj25525@gmail.com>
2026-05-27 13:25:35 +0530

    updated backend render url

--- travelagency-frontend/src/services/api.js
+++ travelagency-frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
 
-    baseURL: "http://localhost:8080"
+    baseURL: "https://saferoutes-travels-fullstack-project.onrender.com"
 });
 
 API.interceptors.request.use(
